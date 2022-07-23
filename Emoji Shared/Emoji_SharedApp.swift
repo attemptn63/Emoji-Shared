e31907aba047ef5b2d362a395d2135b412f4d6ee
attemptn63 <jeongwonho64@gmail.com>
2022-07-23 10:02:15 +0800

    Initial Commit

---
+++ Emoji Shared/Emoji_SharedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_SharedApp.swift
+//  Emoji Shared
+//
+//  Created by T Krobot on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_SharedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
